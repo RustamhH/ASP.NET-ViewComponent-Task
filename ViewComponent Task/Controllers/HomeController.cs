30c28ada52fae6565ba0d0321fe2f3970e3132fd
Rustam Hasanli <rustamh2006@gmail.com>
2024-05-04 23:26:06 +0400

    StepIt Clone Demo

---
+++ ViewComponent Task/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ViewComponent_Task.Models;
+
+namespace ViewComponent_Task.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ViewComponent Task/ViewComponents/MarqueViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson_6_Routing.ViewComponents;
+
+public class MarqueViewComponent : ViewComponent
+{
+
+    public IViewComponentResult Invoke()
+    {
+        var categories = new List<string>()
+        {
+            "Dizayn",
+            "Proqramlaşdırma",
+            "Kibertəhlükəsizlik",
+            "Uşaqlar üçün IT"
+        };
+
+        return View(categories);
+    }
+}
